169123453e37d3e7581afb80604e30210e935b66
andykais <kaisea@rpi.edu>
2016-04-16 02:42:32 +0200

    first test

---
+++ dexi-trakt.tv-scrobbler/dexi_trakt.tv_scrobbler.js
@@ -0,0 +1,17 @@
+// ==UserScript==
+// @name         Dexi Scrobbler
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @match        http://tampermonkey.net/scripts.php
+// @grant        none
+// @include       http://*localhost:8080*
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    // Your code here...
+    console.log('test');
+})();
